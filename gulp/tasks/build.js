955fb7592219bd9f27874b3a8ad28862be43d479
ELK75 <elijah.kajinic@gmail.com>
2018-07-27 11:03:59 -0400

    add build files

---
+++ gulp/tasks/build.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+del = require('del'),
+exec = require('gulp-exec');
+
+gulp.task('deleteDistFolder', function() {
+    return del("./docs");
+});
+
+gulp.task('copyGeneralFiles', ['minCss'], function() {
+    gulp.src('./index.html')
+        .pipe(gulp.dest("./docs"));
+
+    gulp.src('./css/fonts/**/*')
+        .pipe(gulp.dest("./docs/css/fonts"));
+
+    gulp.src('./img/**/*')
+        .pipe(gulp.dest('./docs/img'));
+
+    var cssPathsToCopy = [
+        './css/main.css',
+        './css/icon-font.css',
+    ];
+
+    return gulp.src(cssPathsToCopy)
+        .pipe(gulp.dest("./docs/css"));
+});
+
+gulp.task('minCss', ['deleteDistFolder'], function() {
+    exec('npm run build:css');  
+});
+
+gulp.task('build', ['deleteDistFolder', 'minCss', 'copyGeneralFiles']);
